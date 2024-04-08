38b83d870c272ce99061a66c71b595272a07ad64
Denis Orekhov <3008denis@gmail.com>
2024-04-08 11:34:15 +0500

    Add reverse index to SearchEngine

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -1,20 +1,50 @@
 package hexlet.code;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.regex.Matcher;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.regex.MatchResult;
 
 public class SearchEngine {
+    public static Map<String, List<String>> invertedIndex = new HashMap<>();
+
     public static List<String> search(List<Map<String, String>> docs, String str) {
+
+        buildInvertedIndex(docs);
+
         var map = new HashMap<String, Integer>();
         var terms = str.split(" ");
 
+        for (var term : terms) {
+            var normalizeTerm = Pattern.compile("\\w+")
+                    .matcher(term)
+                    .results()
+                    .map(MatchResult::group)
+                    .collect(Collectors.joining());
+            if (invertedIndex.containsKey(normalizeTerm)) {
+                for (var docId : invertedIndex.get(normalizeTerm)) {
+                    map.put(docId, map.getOrDefault(docId, 0) + 1);
+                }
+            }
+        }
+
+        return map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private static void buildInvertedIndex(List<Map<String, String>> docs) {
+
         for (var doc : docs) {
             var text = doc.get("text");
-            var count = 0;
+            var docId = doc.get("id");
+
+            var terms = text.split("\\s+");
 
             for (var term : terms) {
                 var normalizeTerm = Pattern.compile("\\w+")
@@ -22,25 +52,12 @@ public class SearchEngine {
                         .results()
                         .map(MatchResult::group)
                         .collect(Collectors.joining());
-                Pattern pattern = Pattern.compile(normalizeTerm + "(?=[!?.,\\s]|$)");
-                Matcher matcher = pattern.matcher(text);
-                while (matcher.find()) {
-                    count++;
-                }
-            }
 
-            if (count > 0) {
-                map.put(doc.get("id"), count);
+                invertedIndex.computeIfAbsent(normalizeTerm, k -> new ArrayList<>()).add(docId);
             }
-
         }
 
-        return map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-
+        System.out.println(invertedIndex);
     }
-
 }
 
