5de40e6727a961988a4ab9e15bb100115752a8c7
Denis Orekhov <3008denis@gmail.com>
2024-04-08 13:19:33 +0500

    Add TDF

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -4,19 +4,18 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Comparator;
-import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.regex.MatchResult;
 
 public class SearchEngine {
-    public static Map<String, List<String>> invertedIndex = new HashMap<>();
+    public static Map<String, Map<String, Double>> invertedIndex = new HashMap<>();
+    public static Map<String, Integer> docWordCount = new HashMap<>();
 
     public static List<String> search(List<Map<String, String>> docs, String str) {
-
         buildInvertedIndex(docs);
 
-        var map = new HashMap<String, Integer>();
+        var map = new HashMap<String, Double>();
         var terms = str.split(" ");
 
         for (var term : terms) {
@@ -26,8 +25,10 @@ public class SearchEngine {
                     .map(MatchResult::group)
                     .collect(Collectors.joining());
             if (invertedIndex.containsKey(normalizeTerm)) {
-                for (var docId : invertedIndex.get(normalizeTerm)) {
-                    map.put(docId, map.getOrDefault(docId, 0) + 1);
+                for (var entry : invertedIndex.get(normalizeTerm).entrySet()) {
+                    var docId = entry.getKey();
+                    var tfidf = entry.getValue();
+                    map.put(docId, map.getOrDefault(docId, 0.0) + tfidf);
                 }
             }
         }
@@ -39,7 +40,6 @@ public class SearchEngine {
     }
 
     private static void buildInvertedIndex(List<Map<String, String>> docs) {
-
         for (var doc : docs) {
             var text = doc.get("text");
             var docId = doc.get("id");
@@ -53,11 +53,22 @@ public class SearchEngine {
                         .map(MatchResult::group)
                         .collect(Collectors.joining());
 
-                invertedIndex.computeIfAbsent(normalizeTerm, k -> new ArrayList<>()).add(docId);
+                invertedIndex.computeIfAbsent(normalizeTerm, k -> new HashMap<>())
+                        .merge(docId, 1.0 / terms.length, Double::sum);
+
+                docWordCount.put(docId, docWordCount.getOrDefault(docId, 0) + 1);
             }
         }
 
-        System.out.println(invertedIndex);
+        // Вычисляем TF-IDF для каждого термина в каждом документе
+        for (var termEntry : invertedIndex.entrySet()) {
+            var idf = Math.log((double) docs.size() / termEntry.getValue().size());
+            for (var docEntry : termEntry.getValue().entrySet()) {
+                var tf = docEntry.getValue();
+                var tfidf = tf * idf;
+                termEntry.getValue().put(docEntry.getKey(), tfidf);
+            }
+        }
+
     }
 }
-
